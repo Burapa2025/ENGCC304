69eb7db1a47d28d6b66c6171bd538d609e157d0d
Burapa2025 <guy6176@gmail.com>
2025-07-26 21:38:03 +0700

    lab

--- lab/lab 5/lab5.c
+++ lab/lab 5/lab5.c
@@ -1,51 +1,32 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-
-int isNumber(char *str) {
-    int i = 0;
-    while (str[i] != '\0') {
-        if (!isdigit(str[i])) {
-            return 0;
-        }
-        i++;
-    }
-    return 1;
-}
 
 int main() {
-    int score = 0;
-    char inputBuffer[100];
+    int Grade;
 
-    while (1) {
-        printf("Please enter your score: ");
-        scanf("%s", inputBuffer);
-
-        if (isNumber(inputBuffer)) {
-            sscanf(inputBuffer, "%d", &score);
-            break;
-        } else {
-            printf("please enter number only.\n");
-        }
+    // รับข้อมูลเกรด
+    printf("enter score : \n");
+    if (scanf("%d", &Grade) != 1) {
+        printf("please enter number only.\n");
+    return 0;
     }
 
-    printf("Grade: ");
-    if (score >= 80) {
-        printf("A !\n");
-    } else if (score >= 75) {
-        printf("B+ !\n");
-    } else if (score >= 70) {
-        printf("B !\n");
-    } else if (score >= 65) {
-        printf("C+ !\n");
-    } else if (score >= 60) {
-        printf("C !\n");
-    } else if (score >= 55) {
-        printf("D+ !\n");
-    } else if (score >= 50) {
-        printf("D !\n");
-    } else {
-        printf("F !\n");
+    // ตรวจสอบเกรด
+    if (Grade >= 80) {
+        printf("Grade = A \n");
+    } else if (Grade >= 75) {
+        printf("Grade = B+ \n");
+    } else if (Grade >= 70) {
+        printf("Grade = B \n");
+    } else if (Grade >= 65) {
+        printf("Grade = C+ \n");
+    } else if (Grade >= 60) {
+        printf("Grade = C \n");
+    } else if (Grade >= 55) {
+        printf("Grade = D+ \n");
+    } else if (Grade >= 50) {
+        printf("Grade = D \n");
+    } else if (Grade < 50) {
+        printf("Grade = F \n");
     }
     return 0;
 }
